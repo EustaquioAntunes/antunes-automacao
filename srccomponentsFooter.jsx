80cac9a3c3f4bebde9ec26181355714f15e8468e
EustaquioAntunes <eustaquioasi@gmail.com>
2025-11-16 18:28:58 -0300

    Update srccomponentsFooter.jsx

--- srccomponentsFooter.jsx
+++ srccomponentsFooter.jsx
@@ -1,11 +1,1 @@
-import React from 'react'
 
-export default function Footer(){
-  return (
-    <footer className="site-footer">
-      <div className="container">
-        <p>© 2025 Antunes Automação de Sistemas Industriais. Todos os direitos reservados.</p>
-      </div>
-    </footer>
-  )
-}
